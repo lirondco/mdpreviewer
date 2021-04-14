a522569549fca154e3a7d23d10fb376359a417a8
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-04-14 16:49:59 -0700

    completed the preview component

---
+++ src/Preview.tsx
@@ -0,0 +1,53 @@
+import {
+  faGlasses,
+  faMinusSquare,
+  faPlusCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Tooltip } from "@material-ui/core";
+
+type PreviewProps = {
+  isExpanded: string;
+  text: string;
+  handleExpand: any;
+};
+
+const Preview = ({
+  isExpanded,
+  text,
+  handleExpand,
+}: PreviewProps): JSX.Element => {
+  const shouldExpand = isExpanded === "preview";
+  const icon = shouldExpand ? (
+    <FontAwesomeIcon icon={faMinusSquare} />
+  ) : (
+    <FontAwesomeIcon icon={faPlusCircle} />
+  );
+  const containerClass = shouldExpand ? "window-enlarged" : "preview-window";
+  const toolTip = shouldExpand ? "Minimize" : "Maximize";
+  const ariaLabel = toolTip + " preview window.";
+
+  return (
+    <div className={`container ${containerClass}`}>
+      <div className="title-bar">
+        <div className="window-title">
+          <FontAwesomeIcon icon={faGlasses} /> Preview
+        </div>
+        <Tooltip title={toolTip} arrow>
+          <button
+            className="arrow"
+            aria-label={ariaLabel}
+            onClick={handleExpand}
+          >
+            {icon}{" "}
+          </button>
+        </Tooltip>
+      </div>
+      <div id="preview" dangerouslySetInnerHTML={{ __html: text }}>
+          {" "}
+      </div>
+    </div>
+  );
+};
+
+export default Preview;
