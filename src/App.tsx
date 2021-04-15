7b4e9ea49b820b1cd361e61c3a6a7fdf73fa4ca0
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-04-14 17:03:37 -0700

    completed component compilation, now testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import dompurify from 'dompurify';
+import defaultMarkdown from './defaultMarkDown';
+import marked from 'marked';
+import Editor from './Editor';
+import Preview from './Preview';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = ():JSX.Element => {
+  const [markDown, setMarkDown] = useState(defaultMarkdown);
+  const [expanded, setExpanded] = useState("none");
+  const sanitizer = dompurify.sanitize;
+  const markedDown = sanitizer(marked(markDown, {breaks: true}));
+  
+  const isExpNone = expanded === "none";
+  const toggleExpandEditor = isExpNone ? "editor" : "none";
+  const toggleExpandPreview = isExpNone ? "preview" : "none";
+  const renderEditor = ():JSX.Element => (
+    <Editor 
+      handleChange={(ev:any) => setMarkDown(ev.target.value)}
+      isExpanded={expanded}
+      handleExpand={() => setExpanded(toggleExpandEditor)}
+      text={markDown}
+    />
   );
+  const renderPreview = ():JSX.Element => (
+    <Preview 
+      text={markedDown}
+      isExpanded={expanded}
+      handleExpand={() => setExpanded(toggleExpandPreview)}
+    />
+  );
+  const renderComponents = ():JSX.Element => {
+    if(expanded === "editor") {
+      return renderEditor();
+    }
+    if(expanded === "preview") {
+      return renderPreview();
+    }
+    else {
+      return (
+        <>
+          {renderEditor()}
+          {renderPreview()}
+        </>
+      )
+    }
+  }
+
+  return (
+    <main>
+      {renderComponents()}
+    </main>
+  )
 }
 
 export default App;
